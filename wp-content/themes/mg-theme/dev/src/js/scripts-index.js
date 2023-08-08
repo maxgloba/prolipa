86b092bff958e01866037f30e67c1abc23ed3123
Max Globa <onyx18121990@gmail.com>
2023-08-08 09:13:38 +0300

    added some changed

--- wp-content/themes/mg-theme/dev/src/js/scripts-index.js
+++ wp-content/themes/mg-theme/dev/src/js/scripts-index.js
@@ -3,6 +3,7 @@
 //= b2b/validate.min.js
 
 //= partials/header.js
+//= partials/global.js
 
 
 $(document).ready(function(){
@@ -116,80 +117,6 @@ otherNav.forEach(item => {
   })
 })
 
-const disableScroll = () => {
-  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  window.onscroll = function() {
-    window.scrollTo(scrollLeft, scrollTop);
-  }
-}
-
-const enableScroll = () => {
-  window.onscroll = function() {}
-}
-
-const modalOpen = document.querySelectorAll('.modalOpen')
-modalOpen.forEach(el => {
-  el.addEventListener('click', e => {
-    e.preventDefault()
-    disableScroll()
-    document.querySelector('.modal').classList.add('active')
-  })
-})
-
-const modalClose = document.querySelectorAll('.modalClose')
-modalClose.forEach(el => {
-  el.addEventListener('click', e => {
-    e.preventDefault()
-    enableScroll()
-    document.querySelector('.modal').classList.remove('active')
-  })
-})
-
-// setTimeout(()=>{
-//   document.querySelector('.catalog__tabs').style.minHeight = document.querySelector('.catalog__tab.active').clientHeight+'px'
-// }, 500);
 
 
-/* copyright */
-document.getElementById('copyrightYear').innerHTML = new Date().getFullYear();
 
-const qualityFunc = () => {
-  if(window.innerWidth > 575 && window.innerWidth < 800){
-    const qualityImg = document.querySelector('.quality__item-img img')
-    document.querySelector('.quality__brown').style.marginTop = `-${qualityImg.clientHeight}px`
-  } else {
-    document.querySelector('.quality__brown').style.marginTop = `0`
-  }
-}
-
-setTimeout(()=>{
-  qualityFunc()
-}, 500);
-window.addEventListener('resize', () => {
-  qualityFunc()
-}, true);
-
-setTimeout(()=>{
-  const dataTel = document.querySelectorAll('[data-tel]')
-  dataTel.forEach(tel => {
-    tel.href = `tel:+${tel.dataset.tel.replace(/[^\dA-Z]/g, '').replace(/[\s]/g, '')}`
-  })
-}, 50);
-
-const formPhones = document.querySelectorAll('input[name="phone"]')
-formPhones.forEach(el => {
-  el.addEventListener('input', e => {
-    // +7 (___) ___-__-__
-    let arr = ['+', '7']
-    let number = e.target.value.replace("+7", "")
-    let phoneArr = number.replace(/[^\dA-Z]/g, '').replace(/[\s]/g, '').split('')
-    console.dir(phoneArr)
-    arr.push(...phoneArr)
-    if (arr.length > 0) arr.splice(2, 0, ' (');
-    if (arr.length > 6) arr.splice(6, 0, ') ');
-    if (arr.length > 10) arr.splice(10, 0, '-');
-    if (arr.length > 13) arr.splice(13, 0, '-');
-    e.target.value = arr.toString().replace(/[,]/g, '');
-  })
-})
--- wp-content/themes/mg-theme/js/scripts-privacy.js
+++ wp-content/themes/mg-theme/js/scripts-privacy.js
@@ -882,7 +882,31 @@ document.addEventListener('DOMContentLoaded', function () {
   window.addEventListener("scroll", () => headerScroll())
 
 })();
+document.getElementById('copyrightYear').innerHTML = new Date().getFullYear();
 
+setTimeout(()=>{
+  const dataTel = document.querySelectorAll('[data-tel]')
+  dataTel.forEach(tel => {
+    tel.href = `tel:+${tel.dataset.tel.replace(/[^\dA-Z]/g, '').replace(/[\s]/g, '')}`
+  })
+}, 50);
+
+const formPhones = document.querySelectorAll('input[name="phone"]')
+formPhones.forEach(el => {
+  el.addEventListener('input', e => {
+    // +7 (___) ___-__-__
+    let arr = ['+', '7']
+    let number = e.target.value.replace("+7", "")
+    let phoneArr = number.replace(/[^\dA-Z]/g, '').replace(/[\s]/g, '').split('')
+    console.dir(phoneArr)
+    arr.push(...phoneArr)
+    if (arr.length > 0) arr.splice(2, 0, ' (');
+    if (arr.length > 6) arr.splice(6, 0, ') ');
+    if (arr.length > 10) arr.splice(10, 0, '-');
+    if (arr.length > 13) arr.splice(13, 0, '-');
+    e.target.value = arr.toString().replace(/[,]/g, '');
+  })
+})
 
 const disableScroll = () => {
   scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -914,29 +938,21 @@ modalClose.forEach(el => {
   })
 })
 
-/* copyright */
-document.getElementById('copyrightYear').innerHTML = new Date().getFullYear();
+const qualityFunc = () => {
+  const qualityImg = document.querySelector('.quality__item-img img')
+  if(qualityImg.clientHeight === 0){
+    setTimeout(qualityFunc(), 500);
+  }
+  if(window.innerWidth > 575 && window.innerWidth < 800){
+    document.querySelector('.quality__brown').style.marginTop = `-${qualityImg.clientHeight}px`
+  } else {
+    document.querySelector('.quality__brown').style.marginTop = `0`
+  }
+}
 
 setTimeout(()=>{
-  const dataTel = document.querySelectorAll('[data-tel]')
-  dataTel.forEach(tel => {
-    tel.href = `tel:+${tel.dataset.tel.replace(/[^\dA-Z]/g, '').replace(/[\s]/g, '')}`
-  })
+  qualityFunc()
 }, 50);
-
-const formPhones = document.querySelectorAll('input[name="phone"]')
-formPhones.forEach(el => {
-  el.addEventListener('input', e => {
-    // +7 (___) ___-__-__
-    let arr = ['+', '7']
-    let number = e.target.value.replace("+7", "")
-    let phoneArr = number.replace(/[^\dA-Z]/g, '').replace(/[\s]/g, '').split('')
-    console.dir(phoneArr)
-    arr.push(...phoneArr)
-    if (arr.length > 0) arr.splice(2, 0, ' (');
-    if (arr.length > 6) arr.splice(6, 0, ') ');
-    if (arr.length > 10) arr.splice(10, 0, '-');
-    if (arr.length > 13) arr.splice(13, 0, '-');
-    e.target.value = arr.toString().replace(/[,]/g, '');
-  })
-})
+window.addEventListener('resize', () => {
+  qualityFunc()
+}, true);
